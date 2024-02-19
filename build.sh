d966dabd5e26013892f131ff097174a9b373e88d
samoy <antalois23@gmail.com>
2024-02-19 17:43:09 +0700

    dockerfile

--- build.sh
+++ build.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-
-set -e
-
-if [ $1 == "--push" ]; then
-    WILL_PUSH=1
-else
-    WILL_PUSH=0
-fi
-
-docker buildx build \
-      --platform linux/amd64,linux/arm64 \
-      -t docker/getting-started:latest \
-      $( (( $WILL_PUSH == 1 )) && printf %s '--push' ) .
+docker build -t vnc-desktop .
+docker run -d -p 5900:5900 -p 2222:22 --name vnc-container vnc-desktop
